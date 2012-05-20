56704dc7a30556aba149463b0b8e1634cec43c4d
Sam Thorogood <sam.thorogood@gmail.com>
2012-05-20 13:52:09 +1000

    added -suffix

--- atvplay/main.go
+++ atvplay/main.go
@@ -11,9 +11,8 @@ import (
 	"path/filepath"
 )
 
-const SUFFIX = "/atv.mp4"
-
 var target = flag.String("target", "apple-tv.local:7000", "appletv to connect to")
+var ext = flag.String("ext", "mp4", "extension to serve with")
 
 func main() {
 	flag.Parse()
@@ -21,18 +20,19 @@ func main() {
 		log.Fatalf("fatal; expected filename")
 	}
 	path := flag.Args()[0]
+	suffix := fmt.Sprintf("/atv.%s", *ext)
 
 	// Create a temp folder, and symlink to our target.
 	// TODO: If this is killed with ctrl-c, which is usual behaviour, then the deferred
 	// remove may not occur. Perhaps we need to intercept signals (but this is weird).
 	servepath, _ := ioutil.TempDir("", "aplay")
 	path, _ = filepath.Abs(path)
-	err := os.Symlink(path, servepath+SUFFIX)
+	err := os.Symlink(path, servepath+suffix)
 	if err != nil {
 		panic(err)
 	}
 	defer os.RemoveAll(servepath)
-	log.Printf("created serving symlink: %s", servepath+SUFFIX)
+	log.Printf("created serving symlink: %s", servepath+suffix)
 
 	// Create AppleTVLink.
 	link, err := atvlib.NewAppleTVLink(*target)
@@ -47,7 +47,7 @@ func main() {
 	local.Port = <-portCh
 
 	// Extract complete URL of video.
-	address := fmt.Sprintf("http://%s%s", local.String(), SUFFIX)
+	address := fmt.Sprintf("http://%s%s", local.String(), suffix)
 	log.Printf("serving at: %s", address)
 
 	// Play the file that we're now serving.
